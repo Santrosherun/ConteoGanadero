67c37385fe5e3945fec4d5a3e78144c425f44b7c
Santiago Diaz <santiagodd@uninorte.edu.co>
2025-06-16 17:35:24 -0500

    AGREGAR TRY-CATCH A OPERACIONES CRUD

--- db.js
+++ db.js
@@ -8,17 +8,33 @@ db.version(1).stores({
 
 // Funciones básicas
 export const agregarAnimal = async (animal) => {
-  return await db.animales.add(animal);
+    try {
+        return await db.animales.add(animal);
+    } catch (error) {
+        console.error('Error (AGREGAR):', error)
+    }
 };
 
 export const obtenerAnimales = async () => {
-  return await db.animales.toArray();
+    try {
+        return await db.animales.toArray();
+    } catch (error) {
+        console.error('Error (OBTENER):', error)
+    }
 };
 
 export const eliminarAnimal = async (id) => {
-  return await db.animales.delete(id);
+    try {
+        return await db.animales.delete(id);
+    } catch (error) {
+        console.error('Error (ELIMINAR):', error)
+    }
 };
 
 export const actualizarAnimal = async (id, cambios) => {
-  return await db.animales.update(id, cambios);
+    try {
+        return await db.animales.update(id, cambios);
+    } catch (error) {
+        console.error('Error (ACTUALIZAR):', error)
+    }
 };
